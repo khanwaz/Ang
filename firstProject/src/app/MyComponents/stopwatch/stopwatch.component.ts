94da6610754912c14132312860b4b3eddb0de524
Akram khan <khanwaz@gmail.com>
2021-10-20 16:07:45 +0530

    created stopwatch component
    stopwatch.component.ts
    stopwatch.component.html
    stopwatch.component.css

---
+++ firstProject/src/app/MyComponents/stopwatch/stopwatch.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-stopwatch',
+    templateUrl: './stopwatch.component.html',
+    styleUrls:['./stopwatch.component.html']
+})
+export class StopwatchComponent{}
--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
+import { ServerComponent } from './MyComponents/server/server.component';
+import { StopwatchComponent } from './MyComponents/stopwatch/stopwatch.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    ServerComponent,
+    StopwatchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    //HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
