68dab3f5cb1451c67ca45f47e42aff83a9a7181f
Akram khan <khanwaz@gmail.com>
2022-02-03 20:26:02 +0530

    update

--- projectList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projectList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Apple Pie',
+      'This is the apple recipe',
+      'https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/apple-pie-filling.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
